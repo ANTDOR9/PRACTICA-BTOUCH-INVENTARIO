2230df8080cd850f9717557f8939665a2b310609
ANTDOR9 <1677406@senati.pe>
2026-07-28 21:08:49 -0500

    Capa de Presentación (WPF)

---
+++ VentasInventario.Presentacion/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VentasInventario.Presentacion;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
