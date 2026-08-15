cef3005826f87afb0e590bc504a9e511932b4de3
ANTDOR9 <1677406@senati.pe>
2026-08-14 20:03:41 -0500

    solucion de sincronizacion

--- android/app/src/main/java/com/brighter/pad/MainActivity.java
+++ android/app/src/main/java/com/brighter/pad/MainActivity.java
@@ -1,5 +1,39 @@
 package com.brighter.pad;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
+import android.webkit.WebStorage;
+import android.content.pm.ActivityInfo;
 import com.getcapacitor.BridgeActivity;
+import android.graphics.Color;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Configurar pantalla completa e inmersiva
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        
+        // Ocultar barras de navegación (modo inmersivo)
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+        
+        // Forzar orientación landscape (paisaje) para pantallas 4K
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
